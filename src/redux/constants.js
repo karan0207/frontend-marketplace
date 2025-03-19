f97f30b7d6ae1d7bfde291c184a6a5ec3d7ddf49
Karan0207 <minidox0207@gmail.com>
2025-03-19 21:27:18 +0530

    intial frontend

---
+++ src/redux/constants.js
@@ -0,0 +1,8 @@
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export const USERS_URL = `${apiUrl}/api/users`;
+export const CATEGORY_URL = `${apiUrl}/api/category`;
+export const PRODUCT_URL = `${apiUrl}/api/products`;
+export const UPLOAD_URL = `${apiUrl}/api/upload`;
+export const ORDERS_URL = `${apiUrl}/api/orders`;
+export const PAYPAL_URL = `${apiUrl}/api/config/paypal`;
